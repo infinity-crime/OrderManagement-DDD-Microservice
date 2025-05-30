b15ee3b868971154942f7c019e9851c6f1750f95
Kirill Zhestkov <infinity.crime@outlook.com>
2025-05-30 22:13:21 +0300

    Added immutable class ValueObject. Modified class Order, and implemented one-to-many relationship with class OrderItem, representing a unit of goods.

--- OrderManagement.Domain/Entities/Order.cs
+++ OrderManagement.Domain/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderManagement.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,51 @@ namespace OrderManagement.Domain.Entities
 {
     public class Order
     {
+        // Default fields
+        public Guid Id { get; private set; }
+        public Guid CustomerId { get; private set; }
+        public string Status { get; private set; }
+        public decimal TotalAmount { get; private set; }
+        public string Country { get; private set; }
+        public string City { get; private set; }
+        public string Street { get; private set; }
+        public int Postcode { get; private set; }
 
+        // One-to-many relationship
+        public ICollection<OrderItem> OrderItems { get; } = new List<OrderItem>();
+
+        // Computed property for VO immutability
+        public ShippingAddress ShippingAddress => new ShippingAddress(Country, City, Street, Postcode);
+
+        // Private constructor that excludes creation of empty object in the database (for EF Core)
+        private Order() { } 
+
+        public Order(Guid customerId, ShippingAddress address)
+        {
+            Id = Guid.NewGuid();
+            CustomerId = customerId;
+            Country = address.Country;
+            City = address.City;
+            Street = address.Street;
+            Postcode = address.Postcode;
+
+            Status = "Created";
+            TotalAmount = 0m;
+        }
+
+        public void AddOrderItem()
+        {
+            
+        }
+
+        public void ChangeAddress(ShippingAddress address)
+        {
+            Country = address.Country;
+            City = address.City;
+            Street = address.Street;
+            Postcode = address.Postcode;
+        }
+
+        private decimal CalculateTotal() => OrderItems.Sum(oi => oi.Quantity * oi.UnitPrice);
     }
 }
---
+++ OrderManagement.Domain/Entities/OrderItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class OrderItem
+    {
+        public Guid Id { get; private set; }
+        public Guid ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal UnitPrice { get; private set; }
+
+        // navigation
+        public Guid OrderId { get; private set; }
+        public Order Order { get; private set; } = null!;
+
+        private OrderItem() { }
+
+        public OrderItem(Guid productId, int qty, decimal unitPrice)
+        {
+            if (qty <= 0) 
+                throw new ArgumentOutOfRangeException("The quantity of goods cannot be less than zero", 
+                    nameof(qty));
+
+            if(unitPrice <= 0) 
+                throw new ArgumentOutOfRangeException("The price of a product cannot be less than zero", 
+                    nameof(unitPrice));
+
+            Id = Guid.NewGuid();
+            ProductId = productId;
+            Quantity = qty;
+            UnitPrice = unitPrice;
+        }
+    }
+}
