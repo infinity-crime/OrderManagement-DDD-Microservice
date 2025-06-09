1fdb80aa2b41d774a1abcf006fa6121e04305458
Kirill Zhestkov <infinity.crime@outlook.com>
2025-06-09 16:51:11 +0300

    Edited attributes in DTO classes (Models folder in API layer)

--- OrderManagement.API/Models/CreateAddressRequest.cs
+++ OrderManagement.API/Models/CreateAddressRequest.cs
@@ -5,15 +5,13 @@ namespace OrderManagement.API.Models
 {
     public class CreateAddressRequest
     {
-        [Required(ErrorMessage = "Country field is required")]
         public string Country { get; set; }
 
-        [Required(ErrorMessage = "City field is required")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Street field is required")]
         public string Street { get; set; }
 
+        [Required(ErrorMessage = "PostCode field is required")]
         [Range(1, 999999, ErrorMessage = "PostCode must be a number between 1 and 999999")]
         public int PostCode { get; set; }
     }
